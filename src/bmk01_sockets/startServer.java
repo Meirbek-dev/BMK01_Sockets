129ecca4c6095de504cf5f09251f5ed5529f6e9e
Meirbek <51261176+Meirbek-dev@users.noreply.github.com>
2021-01-28 12:49:07 +0600

    Add files via upload

---
+++ src/bmk01_sockets/startServer.java
@@ -0,0 +1,11 @@
+package bmk01_sockets;
+
+import java.io.IOException;
+
+public class startServer {
+
+    public static void main(String[] args) throws IOException {
+        MyServer server = new MyServer();
+        server.start();
+    }
+}
